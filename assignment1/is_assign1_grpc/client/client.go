31b1c4fea2c9d01bc34e34458aa1177618177ca5
João Soares <jmsoares@student.dei.uc.pt>
2019-09-28 14:57:54 +0100

    [grpc] - initial gRPC call setup - #1

--- assignment1/is_assign1_grpc/client/client.go
+++ assignment1/is_assign1_grpc/client/client.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"context"
 	"google.golang.org/grpc"
 	"log"
+	"time"
 
 	ss "../message_protocol"
 )
 
 const serverAddr = "127.0.0.1:10000"
 
-func printSearchResults(client ss.SearchServiceClient) {
+func printSearchResults(client ss.SearchServiceClient, owners []ss.Owner) {
 
 }
 
@@ -27,6 +29,22 @@ func main() {
 	client := ss.NewSearchServiceClient(conn)
 
 	//TODO Get owners whom to search for cars -> build objects to memory
+	var owners []ss.Owner
+
+	owners = append(owners,
+		ss.Owner{Uid: 0},
+		ss.Owner{Uid: 1},
+		ss.Owner{Uid: 2},
+		ss.Owner{Uid: 3},
+		ss.Owner{Uid: 4},
+		ss.Owner{Uid: 5})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	_, err = client.GetCarsForOwner(ctx, &ss.SearchRequest{Owners: nil})
+
+	printSearchResults(client, owners)
+
 	//TODO Time time to marshall all information
 	//TODO Send request to server
 	//TODO Parse answer & print to stdout / file
