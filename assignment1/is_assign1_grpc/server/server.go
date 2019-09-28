d04308d6d540a03f8ee0ffea642e2ebbc8b05f57
João Soares <jmsoares@student.dei.uc.pt>
2019-09-28 14:48:34 +0100

    [grpc] - coded initial structure of server - #4

--- assignment1/is_assign1_grpc/server/server.go
+++ assignment1/is_assign1_grpc/server/server.go
@@ -1,5 +1,50 @@
+//go:generate protoc -I ../message_protocol --go_out=plugins=grpc:../message_protocol ../message_protocol/message_protocol.proto
+
 package main
 
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	ss "../message_protocol"
+)
+
+const serverAddr = "127.0.0.1:10000"
+
+type searchServiceServer struct {
+	cars []*ss.Car
+}
+
+func (s *searchServiceServer) GetCarsForOwner(context.Context, *ss.SearchRequest) (*ss.SearchResponse, error) {
+	//TODO Get cars for the requested owners and return info
+	panic("implement me")
+}
+
+func (s *searchServiceServer) loadVehicles() {
+	//TODO Load vehicles to mem here
+	panic("implement me")
+}
+
+func newServer() *searchServiceServer {
+	var s searchServiceServer
+	s.loadVehicles()
+	return &s
+}
+
 func main() {
+
+	lis, err := net.Listen("tcp", serverAddr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+
+	grpcServer := grpc.NewServer(opts...)
+
+	ss.RegisterSearchServiceServer(grpcServer, newServer())
+
+	_ = grpcServer.Serve(lis)
 
 }
