d6e5b29c7efdeae015b3f941f394a9fde0c804e7
João Soares <jmsoares@student.dei.uc.pt>
2019-09-28 15:59:59 +0100

    [XML] - added connection close after sending all information - #3

--- assignment1/is-assign1-xml/server/server.go
+++ assignment1/is-assign1-xml/server/server.go
@@ -10,34 +10,35 @@ import (
 	"os"
 )
 
-const(port = ":4321")
+const (
+	port = ":4321"
+)
 
 func main() {
 
-	o := message_protocol.Owner{Uid: 1, Name: "Zé", Telephone: 1, Address:"a", LicenceNumber:1, IdNumber:1, TaxNumber:1,
+	o := message_protocol.Owner{Uid: 1, Name: "Zé", Telephone: 1, Address: "a", LicenceNumber: 1, IdNumber: 1, TaxNumber: 1,
 		Cars: []message_protocol.Car{
 			{
-				Uid: 1,
-				Brand: "a",
-				Model: "b",
-				BodyType: "c",
-				Approval: "d",
-				Chassis: "e",
-				TyreSize: "1",
-				Weight: 6,
-				Fuel: "gas",
-				Seats: 5,
-				Noise: 2,
+				Uid:          1,
+				Brand:        "a",
+				Model:        "b",
+				BodyType:     "c",
+				Approval:     "d",
+				Chassis:      "e",
+				TyreSize:     "1",
+				Weight:       6,
+				Fuel:         "gas",
+				Seats:        5,
+				Noise:        2,
 				Co2Emissions: 2.4,
-				EngineSize: 1,
-				Power: 5,
-				Color: "Black",
-				Consumption: 3.2,
-				Plate: "as-23-re",
-				Date: "10",
-				OwnerUid: 1,
+				EngineSize:   1,
+				Power:        5,
+				Color:        "Black",
+				Consumption:  3.2,
+				Plate:        "as-23-re",
+				Date:         "10",
+				OwnerUid:     1,
 			}}}
-
 
 	ln, err := net.Listen("tcp", port)
 	if err != nil {
@@ -53,6 +54,7 @@ func main() {
 	}
 
 	for {
+		fmt.Println("W8ing for connection")
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatalf("could not accept: %v", err)
@@ -60,8 +62,10 @@ func main() {
 
 		message, _ := bufio.NewReader(conn).ReadString('\n')
 
-		fmt.Printf(string(message))
+		fmt.Printf("Received from client -> %s\n", string(message))
+		fmt.Print("Sending -> ")
 		_, _ = os.Stdout.Write(output)
 		_, _ = conn.Write(output)
+		_ = conn.Close()
 	}
 }
