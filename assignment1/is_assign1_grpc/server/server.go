98025fd92f9bce66a1488e4e320c2712e635c956
João Soares <jmsoares@student.dei.uc.pt>
2019-09-28 17:09:56 +0100

    [grpc] - request / response communication with dummy data complete - #4

--- assignment1/is_assign1_grpc/server/server.go
+++ assignment1/is_assign1_grpc/server/server.go
@@ -18,16 +18,122 @@ type searchServiceServer struct {
 	cars []*ss.Car
 }
 
-func (s *searchServiceServer) GetCarsForOwner(context.Context, *ss.SearchRequest) (*ss.SearchResponse, error) {
-	//TODO Get cars for the requested owners and return info
-	//panic("implement me")
-	fmt.Println("Testing communication")
-	return nil, nil
+func (s *searchServiceServer) GetCarsForOwner(ctx context.Context, searchRequest *ss.SearchRequest) (*ss.SearchResponse, error) {
+
+	fmt.Print("Received request for cars of: ")
+
+	for _, owner := range searchRequest.Owners {
+		fmt.Printf("%d ", owner.Uid)
+	}
+
+	r := ss.SearchResponse{Owners: nil}
+	r.Owners = append(r.Owners, &ss.Owner{
+		Uid:           1,
+		Name:          "Ivanovsky aka 4 legged",
+		Telephone:     123123123,
+		Address:       "radioactive street, chernobyl",
+		LicenceNumber: 124352,
+		IdNumber:      025131,
+		TaxNumber:     3212120,
+		Cars:          s.cars,
+	},
+		&ss.Owner{
+			Uid:           1,
+			Name:          "aka 4 legged",
+			Telephone:     123123123,
+			Address:       "radioactive street, chernobyl",
+			LicenceNumber: 124352,
+			IdNumber:      025131,
+			TaxNumber:     3212120,
+			Cars:          s.cars,
+		},
+		&ss.Owner{
+			Uid:           1,
+			Name:          "4 legged",
+			Telephone:     123123123,
+			Address:       "radioactive street, chernobyl",
+			LicenceNumber: 124352,
+			IdNumber:      025131,
+			TaxNumber:     3212120,
+			Cars:          s.cars,
+		},
+		&ss.Owner{
+			Uid:           1,
+			Name:          "legged",
+			Telephone:     123123123,
+			Address:       "radioactive street, chernobyl",
+			LicenceNumber: 124352,
+			IdNumber:      025131,
+			TaxNumber:     3212120,
+			Cars:          s.cars,
+		})
+	return &r, nil
 }
 
 func (s *searchServiceServer) loadVehicles() {
-	//TODO Load vehicles to mem here
-	//panic("implement me")
+	s.cars = append(s.cars, &ss.Car{
+		Uid:          12,
+		Brand:        "Toyota",
+		Model:        "Corolla",
+		BodyType:     "Van",
+		Approval:     "3d24afs563e2xx021",
+		Chassis:      "fas353132wesx00",
+		TyreSize:     "205/55R16",
+		Weight:       850,
+		Fuel:         "Diesel",
+		Seats:        2,
+		Noise:        50,
+		Co2Emissions: 20,
+		EngineSize:   2000,
+		Power:        50,
+		Color:        "Black",
+		Consumption:  5.5,
+		Plate:        "AB12CD",
+		Date:         "20/20/2020",
+		OwnerUid:     1,
+	},
+		&ss.Car{
+			Uid:          12,
+			Brand:        "Toyota1",
+			Model:        "Corolla",
+			BodyType:     "Van",
+			Approval:     "3d24afs563e2xx021",
+			Chassis:      "fas353132wesx00",
+			TyreSize:     "205/55R16",
+			Weight:       850,
+			Fuel:         "Diesel",
+			Seats:        2,
+			Noise:        50,
+			Co2Emissions: 20,
+			EngineSize:   2000,
+			Power:        50,
+			Color:        "Black",
+			Consumption:  5.5,
+			Plate:        "AB12CD",
+			Date:         "20/20/2020",
+			OwnerUid:     1,
+		},
+		&ss.Car{
+			Uid:          12,
+			Brand:        "Toyota2",
+			Model:        "Corolla",
+			BodyType:     "Van",
+			Approval:     "3d24afs563e2xx021",
+			Chassis:      "fas353132wesx00",
+			TyreSize:     "205/55R16",
+			Weight:       850,
+			Fuel:         "Diesel",
+			Seats:        2,
+			Noise:        50,
+			Co2Emissions: 20,
+			EngineSize:   2000,
+			Power:        50,
+			Color:        "Black",
+			Consumption:  5.5,
+			Plate:        "AB12CD",
+			Date:         "20/20/2020",
+			OwnerUid:     1,
+		})
 }
 
 func newServer() *searchServiceServer {
