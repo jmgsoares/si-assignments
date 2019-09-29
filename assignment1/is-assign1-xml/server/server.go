8727253b6e0f4e52b90dd94dcde79e57ebf102b2
João Moreira <jmoreirawk@outlook.pt>
2019-09-29 21:18:16 +0100

    [XML] - Added some TODOs and cleaned the code - #3

--- assignment1/is-assign1-xml/server/server.go
+++ assignment1/is-assign1-xml/server/server.go
@@ -2,33 +2,23 @@ package main
 
 import (
 	mp "../message_protocol"
+	file "../util"
 	"bufio"
-	"encoding/json"
 	"encoding/xml"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
-	"os"
 )
 
-const (
-	port = ":4321"
-)
+const serverAddr = "127.0.0.1:10000"
 
 func GetCarsFromOwners(message string) mp.Owners {
 	result := mp.Owners{}
-	o := loadData("testdata/sampleData.json")
+	o := file.LoadData("testdata/sampleData.json")
 	query := mp.Owners{}
 
 	_ = xml.Unmarshal([]byte(message), &query)
-
-	fmt.Println(query)
-
-	fmt.Print("Received request for cars of owners id's: ")
-	for _, owner := range query.Owners {
-		fmt.Printf("%d ", owner.Uid)
-	}
+	// TODO: get elapsed time here
 
 	for _, owner := range query.Owners {
 		result.Owners = append(result.Owners, o.Owners[owner.Uid-1])
@@ -37,48 +27,29 @@ func GetCarsFromOwners(message string) mp.Owners {
 	return result
 }
 
-func loadData(filePath string) mp.Owners {
-	var data mp.Owners
-
-	fileData, err := ioutil.ReadFile(filePath)
-
-	if err != nil {
-		log.Fatalf("Failed to load sample fileData: %v", err)
-	}
-
-	if err := json.Unmarshal(fileData, &data); err != nil {
-		log.Fatalf("Failed to load sample fileData: %v", err)
-	}
-
-	return data
-}
-
 func main() {
-	ln, err := net.Listen("tcp", port)
+	ln, err := net.Listen("tcp", serverAddr)
 	if err != nil {
 		log.Fatalf("could not connect: %v", err)
 	}
-
 	for {
-		fmt.Println("W8ing for connection")
+
 		conn, err := ln.Accept()
 		if err != nil {
 			log.Fatalf("could not accept: %v", err)
 		}
 
 		message, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Printf("Received from client -> %s\n", message)
 
 		output := GetCarsFromOwners(message)
 
+		// TODO: start elapsed2 time here
 		result, err := xml.MarshalIndent(output, "  ", "    ")
 
 		if err != nil {
 			fmt.Printf("error: %v\n", err)
 		}
 
-		fmt.Print("Sending -> ")
-		_, _ = os.Stdout.Write(result)
 		_, _ = conn.Write(result)
 		_ = conn.Close()
 	}
