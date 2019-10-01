05b2e7c11298e05fb35083802d0cfe97c085f47d
João Soares <joao@soares.pro>
2019-10-01 19:37:38 +0100

    [json] - added code to profile time | added test results for sample data

--- assignment1/json/client/client.go
+++ assignment1/json/client/client.go
@@ -3,18 +3,21 @@ package main
 import (
 	mp "../protocol"
 	file "../util"
+	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net"
+	"os"
+	"strconv"
 	"time"
 )
 
 const serverAddr = "127.0.0.1:10001"
 
-func getVehiclesFromQueryList(owners mp.Owners) time.Duration {
+func getVehiclesFromQueryList(owners mp.Owners) (time.Duration, int) {
 	conn, err := net.Dial("tcp", serverAddr)
 	if err != nil {
 		log.Fatalf("failed to dial: %v", err)
@@ -33,12 +36,12 @@ func getVehiclesFromQueryList(owners mp.Owners) time.Duration {
 
 	if err != nil {
 		fmt.Printf("error: %v", err)
-		return 0
+		return 0, 0
 	}
 	_, err = fmt.Fprintf(conn, "\n")
 	if err != nil {
 		fmt.Printf("error: %v", err)
-		return 0
+		return 0, 0
 	}
 
 	o := mp.Response{}
@@ -55,36 +58,72 @@ func getVehiclesFromQueryList(owners mp.Owners) time.Duration {
 
 	if err != nil {
 		fmt.Printf("error in unmarsheling: %v", err)
-		return 0
+		return 0, 0
 	}
 
 	total := o.Elapsed + elapsed2
-	fmt.Println()
-	fmt.Print(o.Elapsed)
-	fmt.Printf(" ")
-	fmt.Println(elapsed2)
-	fmt.Printf("Time total -> ")
-	fmt.Println(total.Seconds())
-	fmt.Printf("Payload Size -> ")
-	fmt.Println(float64(buf.Len()) / 1024.0 / 1024.0)
-	fmt.Println()
 
-	return total
+	/*
+		fmt.Println()
+		fmt.Print(o.Elapsed)
+		fmt.Printf(" ")
+		fmt.Println(elapsed2)
+		fmt.Printf("Time total -> ")
+		fmt.Println(total)
+		fmt.Printf("Payload Size -> ")
+		fmt.Println(float64(buf.Len()) / 1024.0 / 1024.0)
+		fmt.Println()
+	*/
+	return total, buf.Len()
 }
 
 func main() {
-	owners := file.LoadData("testdata/sampleClientQueryBig.json")
+	querieList := []string{"100", "400", "850", "1700", "3400", "6785"}
+	runList := []int{100, 200, 300, 400, 500}
 
-	getVehiclesFromQueryList(owners)
+	for _, numberRuns := range runList {
 
-	var total time.Duration
+		for _, fileName := range querieList {
 
-	for i := 0; i < 50; i++ {
-		total += getVehiclesFromQueryList(owners)
+			println("Testing " + strconv.Itoa(numberRuns) + " runs with " + fileName + " queries")
+
+			owners := file.LoadData("testdata/" + fileName + "_queries_4.json")
+			getVehiclesFromQueryList(owners)
+			var total time.Duration
+			var payloadSize int
+			for i := 0; i < numberRuns; i++ {
+				ttotal, tpayloadSize := getVehiclesFromQueryList(owners)
+				total += ttotal
+				payloadSize += tpayloadSize
+			}
+
+			total = total / time.Duration(numberRuns)
+			payloadSize = payloadSize / numberRuns
+			fmt.Printf("Mean total -> ")
+			fmt.Println(total)
+			fmt.Printf("Mean PayloadSize -> ")
+			fmt.Println(float32(payloadSize) / 1024.0 / 1024.0)
+			fmt.Println()
+
+			f, err := os.Create("results/" + fileName + "_" + strconv.Itoa(numberRuns) + ".result")
+			if err != nil {
+				log.Fatalf("failed connection to server: %v", err)
+				return
+			}
+
+			w := bufio.NewWriter(f)
+
+			_, err = fmt.Fprintf(w, "%f %f\n", float64(total)/float64(time.Millisecond), float32(payloadSize)/1024.0/1024.0)
+			if err != nil {
+				log.Fatalf("failed connection to server: %v", err)
+				return
+			}
+			err = w.Flush()
+			if err != nil {
+				log.Fatalf("failed connection to server: %v", err)
+				return
+			}
+			f.Close()
+		}
 	}
-
-	total = total / 50
-	fmt.Println()
-	fmt.Printf("Mean total -> ")
-	fmt.Println(total.Milliseconds())
 }
