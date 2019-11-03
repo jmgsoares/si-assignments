75d529c781e975bc03656f3ac68b239725d29dee
João Moreira <jmoreirawk@outlook.pt>
2019-11-03 17:33:04 +0000

    wip - header and common file updated | moved home and profile to the root of webapp for testing

--- assignment2/mybay/common/src/main/java/pt/onept/mei/is1920/mybay/common/util/CountryConverter.java
+++ assignment2/mybay/common/src/main/java/pt/onept/mei/is1920/mybay/common/util/CountryConverter.java
@@ -21,29 +21,29 @@ public class CountryConverter implements AttributeConverter <Country, String> {
 	public static String CountryToString(Country country) {
 		switch (country) {
 			case PT:
-				return "portugal";
+				return "Portugal";
 			case S:
-				return "spain";
+				return "Spain";
 			case F:
-				return "france";
+				return "France";
 			case GB:
-				return "great britain";
+				return "Great Britain";
 			case B:
-				return "belgium";
+				return "Belgium";
 			case CH:
-				return "switzerland";
+				return "Switzerland";
 			default:
 				return null;
 		}
 	}
 
 	public static Country StringToCountry(String s) {
-		if (s.equals("portugal")) return Country.PT;
-		if (s.equals("spain")) return Country.S;
-		if (s.equals("france")) return Country.F;
-		if (s.equals("great britain")) return Country.GB;
-		if (s.equals("belgium")) return Country.B;
-		if (s.equals("switzerland")) return Country.CH;
+		if (s.equals("Portugal")) return Country.PT;
+		if (s.equals("Spain")) return Country.S;
+		if (s.equals("France")) return Country.F;
+		if (s.equals("Great Britain")) return Country.GB;
+		if (s.equals("Belgium")) return Country.B;
+		if (s.equals("Switzerland")) return Country.CH;
 		else return null;
 	}
 }
