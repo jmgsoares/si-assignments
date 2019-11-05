f1bb7862db070876d8acbb58e62e820d40f43c58
João Soares <jmsoares@student.dei.uc.pt>
2019-11-05 21:47:51 +0000

    added cloudinary dependency and test method for file upload

--- assignment2/mybay/web/src/main/java/pt/onept/mei/is1920/mybay/web/controller/ItemController.java
+++ assignment2/mybay/web/src/main/java/pt/onept/mei/is1920/mybay/web/controller/ItemController.java
@@ -1,20 +1,51 @@
 package pt.onept.mei.is1920.mybay.web.controller;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import lombok.Getter;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pt.onept.mei.is1920.mybay.common.contract.ItemEJBRemote;
 
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.servlet.http.Part;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.Serializable;
+import java.nio.file.Files;
 
 @Named(value = "itemController")
 @RequestScoped
 @Getter
 @Setter
 public class ItemController implements Serializable {
+	private static final Logger logger = LoggerFactory.getLogger(ItemController.class);
+
+	private static final Cloudinary imageHandler = new Cloudinary(
+			ObjectUtils.asMap("cloud_name", "onept"));
+
+	private Part uploadedPicture;
+
 	@EJB
 	private ItemEJBRemote item;
+
+	public void saveFile(){
+
+		try (InputStream input = uploadedPicture.getInputStream()) {
+			byte[] picture = input.readAllBytes();
+			imageHandler.uploader().upload(picture,
+					ObjectUtils.asMap(
+							"use_filename", "false",
+							"unique_filename", "true"));
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
