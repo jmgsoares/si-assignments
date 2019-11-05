61c375d59ae34da4c17b8b58c0737945d5d2276e
João Moreira <jmoreirawk@outlook.pt>
2019-11-05 23:30:15 +0000

    Front-end add item and search item

---
+++ assignment2/mybay/web/src/main/webapp/resources/js/formatDatePicker.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".datepicker").datepicker({
+        dateFormat: 'dd MM, yy',
+        changeMonth: true,
+        changeYear: true,
+        yearRange: "2018:2019"
+    });
+});
