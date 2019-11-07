58a335eff474c4514afbda08055452e334408dcd
João Soares <jmsoares@student.dei.uc.pt>
2019-11-07 15:52:17 +0000

    Added search filter controller & updated search type to match structure

---
+++ assignment2/mybay/web/src/main/java/pt/onept/mei/is1920/mybay/web/controller/SearchFilterController.java
@@ -0,0 +1,27 @@
+package pt.onept.mei.is1920.mybay.web.controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import pt.onept.mei.is1920.mybay.common.enums.SearchFilter;
+import pt.onept.mei.is1920.mybay.common.utility.SearchFilterConverter;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+
+@Named(value = "searchFilterController")
+@RequestScoped
+@Getter
+@Setter
+public class SearchFilterController {
+
+	public List<String> getSearchFilters() {
+		List<String> searchFilters = new ArrayList<>();
+		for (SearchFilter c : SearchFilter.values()) {
+			String searchFilter = SearchFilterConverter.SearchFilterToString(c);
+			if(searchFilter != null) searchFilters.add(searchFilter);
+		}
+		return searchFilters;
+	}
+}
