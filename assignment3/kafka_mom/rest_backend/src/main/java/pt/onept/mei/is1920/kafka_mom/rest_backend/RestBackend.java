10928c0477af3479ede4622695b33b43f845eacb
João Soares <jmsoares@student.dei.uc.pt>
2019-11-17 22:31:41 +0000

    mapped /health probing requests and root request #56

--- assignment3/kafka_mom/rest_backend/src/main/java/pt/onept/mei/is1920/kafka_mom/rest_backend/RestBackend.java
+++ assignment3/kafka_mom/rest_backend/src/main/java/pt/onept/mei/is1920/kafka_mom/rest_backend/RestBackend.java
@@ -1,7 +1,23 @@
 package pt.onept.mei.is1920.kafka_mom.rest_backend;
 
-public class RestBackend {
-	public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.onept.mei.is1920.kafka_mom.rest_backend.handler.HealthProbeHandler;
 
+import static spark.Spark.*;
+
+public class RestBackend {
+	private static Logger logger = LoggerFactory.getLogger(RestBackend.class);
+
+	public static void main(String[] args) {
+		logger.info("Starting Backend");
+		port(8080);
+		get("/", (req, res) -> {
+			logger.info("Handling root request from " + req.ip() + " " + req.userAgent());
+			return RestBackend.class.toString();
+		});
+		path("/health", () -> {
+			get("", new HealthProbeHandler());
+		});
 	}
 }
