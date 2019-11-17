f9dcdc96f712bc1976dfabefe8d3cbf3b9754603
João Soares <jmsoares@student.dei.uc.pt>
2019-11-17 22:29:51 +0000

    Added health probe class to test rest backend #56

---
+++ assignment3/kafka_mom/rest_backend/src/main/java/pt/onept/mei/is1920/kafka_mom/rest_backend/handler/HealthProbeHandler.java
@@ -0,0 +1,17 @@
+package pt.onept.mei.is1920.kafka_mom.rest_backend.handler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class HealthProbeHandler implements Route {
+	private static Logger logger = LoggerFactory.getLogger(HealthProbeHandler.class);
+
+	@Override
+	public Object handle(Request request, Response response) {
+		logger.info("Handling health request status from " + request.ip() + " " + request.userAgent());
+		return "alive";
+	}
+}
