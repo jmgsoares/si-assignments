7498da3786957de42cf1bd6fcdf0294fe247eff9
João Soares <jmsoares@student.dei.uc.pt>
2019-11-17 19:10:12 +0000

    added test connector and db connection - #55

--- assignment3/docker/kafka_2.12-2.3.1/start.sh
+++ assignment3/docker/kafka_2.12-2.3.1/start.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh /kafka/bin/zookeeper-server-start.sh /kafka/config/zookeeper.properties &
-sh /kafka/bin/kafka-server-start.sh /kafka/config/server.properties
+sh /kafka/bin/kafka-server-start.sh /kafka/config/server.properties &
+sh /kafka/bin/connect-standalone.sh /kafka/config/connect-standalone.properties /kafka/config/connect-jdbc-source.properties 
