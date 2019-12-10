fb56a20076cc9b1e09b6b48b57262afb2cd1ba1f
João Soares <jmsoares@student.dei.uc.pt>
2019-12-10 01:50:10 +0000

    updated orders processing to meet project spec and new changes to jdbc source connector

--- assignment3/kafka/orders/src/main/java/pt/onept/mei/is1920/assignment/kafka/orders/Orders.java
+++ assignment3/kafka/orders/src/main/java/pt/onept/mei/is1920/assignment/kafka/orders/Orders.java
@@ -14,21 +14,21 @@ public class Orders {
 
 	public static void main(String[] args) {
 
-		String itemsTopic = "kafkaShop.items";
-		String countriesTopic = "kafkaShop.countries";
+		String sourceTopic = "DBInfo";
 
 		java.util.Properties props = new Properties();
 		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-orders-app");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 
 		StreamsBuilder builder = new StreamsBuilder();
 
-		KStream<String, String> itemsStream = builder.stream(itemsTopic);
+		KStream<String, String> DbInfoKStream = builder.stream(sourceTopic);
 
-		itemsStream.foreach((s, s2) -> {
-			JsonObject jsonObject = JsonParser.parseString(s2).getAsJsonObject();
-			System.out.println(jsonObject.getAsJsonObject("payload").get("name").toString().replace("\"",""));
+		DbInfoKStream.foreach((k, v) -> {
+			System.out.print(k + " ");
+			System.out.println(v);
 		});
 
 		KafkaStreams streams = new KafkaStreams(builder.build(), props);
