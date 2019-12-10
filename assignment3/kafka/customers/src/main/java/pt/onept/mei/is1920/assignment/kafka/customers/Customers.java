938ce53c0fd7a9a5cef372ebbf13df0ed2267ccf
João Soares <joao@soares.pro>
2019-12-10 12:29:25 +0000

    added function and trigger to add timestamp when data is updated

--- assignment3/kafka/customers/src/main/java/pt/onept/mei/is1920/assignment/kafka/customers/Customers.java
+++ assignment3/kafka/customers/src/main/java/pt/onept/mei/is1920/assignment/kafka/customers/Customers.java
@@ -11,12 +11,13 @@ import pt.onept.mei.is1920.assignment.kafka.common.type.Country;
 import java.util.Properties;
 
 public class Customers {
+
 	public static void main(String[] args) {
 
-		String sourceTopic = "DBInfo";
+		String sourceTopic = "dbinfo";
 
 		java.util.Properties props = new Properties();
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-orders-app");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-customers-app");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
--- assignment3/tables-sample-data.sql
+++ assignment3/tables-sample-data.sql
@@ -1,32 +1,45 @@
-create table data
-(
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+RETURNS TRIGGER AS $$
+BEGIN
+  NEW.updated = NOW();
+  RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+create table info(
     id   SERIAL,
     type VARCHAR(40) NOT NULL,
     name VARCHAR(40) NOT NULL,
+    updated TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     PRIMARY KEY (id)
 );
 
-INSERT INTO data (type, name) VALUES ('country', 'France');
-INSERT INTO data (type, name) VALUES ('country', 'Poland');
-INSERT INTO data (type, name) VALUES ('country', 'United Kingdom');
-INSERT INTO data (type, name) VALUES ('country', 'Italy');
-INSERT INTO data (type, name) VALUES ('country', 'Portugal');
-INSERT INTO data (type, name) VALUES ('country', 'Spain');
-INSERT INTO data (type, name) VALUES ('country', 'Switzerland');
-INSERT INTO data (type, name) VALUES ('country', 'Netherlands');
-INSERT INTO data (type, name) VALUES ('country', 'Ireland');
-INSERT INTO data (type, name) VALUES ('country', 'Luxembourg');
-INSERT INTO data (type, name) VALUES ('country', 'Deutschland');
-INSERT INTO data (type, name) VALUES ('item', 'ChainSaw');
-INSERT INTO data (type, name) VALUES ('item', 'Car');
-INSERT INTO data (type, name) VALUES ('item', 'Microwave');
-INSERT INTO data (type, name) VALUES ('item', 'CellPhone');
-INSERT INTO data (type, name) VALUES ('item', 'Cow');
-INSERT INTO data (type, name) VALUES ('item', 'Beef');
-INSERT INTO data (type, name) VALUES ('item', 'Brick');
-INSERT INTO data (type, name) VALUES ('item', 'Wood');
-INSERT INTO data (type, name) VALUES ('item', 'GoldenToilet');
-INSERT INTO data (type, name) VALUES ('item', 'Castle');
-INSERT INTO data (type, name) VALUES ('item', 'Hamburger');
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON info
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+INSERT INTO info (type, name) VALUES ('country', 'France');
+INSERT INTO info (type, name) VALUES ('country', 'Poland');
+INSERT INTO info (type, name) VALUES ('country', 'United Kingdom');
+INSERT INTO info (type, name) VALUES ('country', 'Italy');
+INSERT INTO info (type, name) VALUES ('country', 'Portugal');
+INSERT INTO info (type, name) VALUES ('country', 'Spain');
+INSERT INTO info (type, name) VALUES ('country', 'Switzerland');
+INSERT INTO info (type, name) VALUES ('country', 'Netherlands');
+INSERT INTO info (type, name) VALUES ('country', 'Ireland');
+INSERT INTO info (type, name) VALUES ('country', 'Luxembourg');
+INSERT INTO info (type, name) VALUES ('country', 'Deutschland');
+INSERT INTO info (type, name) VALUES ('item', 'ChainSaw');
+INSERT INTO info (type, name) VALUES ('item', 'Car');
+INSERT INTO info (type, name) VALUES ('item', 'Microwave');
+INSERT INTO info (type, name) VALUES ('item', 'CellPhone');
+INSERT INTO info (type, name) VALUES ('item', 'Cow');
+INSERT INTO info (type, name) VALUES ('item', 'Beef');
+INSERT INTO info (type, name) VALUES ('item', 'Brick');
+INSERT INTO info (type, name) VALUES ('item', 'Wood');
+INSERT INTO info (type, name) VALUES ('item', 'GoldenToilet');
+INSERT INTO info (type, name) VALUES ('item', 'Castle');
+INSERT INTO info (type, name) VALUES ('item', 'Hamburger');
 
 
