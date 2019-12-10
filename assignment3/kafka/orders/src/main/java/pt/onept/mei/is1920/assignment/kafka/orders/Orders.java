f1e867c062e280a64c20bca0485988b7153bb501
João Soares <jmsoares@student.dei.uc.pt>
2019-12-10 18:30:57 +0000

    added producer properties and code to write purchase to sink topic

--- assignment3/kafka/orders/src/main/java/pt/onept/mei/is1920/assignment/kafka/orders/Orders.java
+++ assignment3/kafka/orders/src/main/java/pt/onept/mei/is1920/assignment/kafka/orders/Orders.java
@@ -1,6 +1,9 @@
 package pt.onept.mei.is1920.assignment.kafka.orders;
 
 import com.google.gson.Gson;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -9,10 +12,12 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pt.onept.mei.is1920.assignment.kafka.common.type.Item;
+import pt.onept.mei.is1920.assignment.kafka.common.type.Order;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -24,15 +29,27 @@ public class Orders {
 	public static void main(String[] args) {
 
 		String sourceTopic = "DBInfo";
+		String sinkTopic = "Purchases";
 
-		Map<Long, String> itemsMap = new HashMap<>();
+		Map<Long, Item> itemsMap = new HashMap<>();
 
-		java.util.Properties props = new Properties();
-		//props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-orders-app");
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-orders-test-app-1");
-		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
-		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+		java.util.Properties sourceProps = new Properties();
+		//sourceProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-orders-app");
+		sourceProps.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-orders-test-app-4");
+		sourceProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		sourceProps.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+		sourceProps.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+		Properties sinkProps = new Properties();
+		sinkProps.put("bootstrap.servers", "localhost:9092");
+		sinkProps.put("acks", "all");
+		sinkProps.put("retries", 0);
+		sinkProps.put("batch.size", 16384);
+		sinkProps.put("linger.ms", 1);
+		sinkProps.put("buffer.memory", 33554432);
+		sinkProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		sinkProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		Producer<String, String> producer = new KafkaProducer<>(sinkProps);
 
 		StreamsBuilder builder = new StreamsBuilder();
 
@@ -44,26 +61,31 @@ public class Orders {
 			Item item = gson.fromJson(v, Item.class);
 			if(itemsMap.containsKey(item.getId())) {
 				logger.info("Updating item " + item.getId());
-				itemsMap.replace(item.getId(), item.getName());
+				itemsMap.replace(item.getId(), item);
 			}
 			else {
 				logger.info("Adding new item " + item.getId() + " " + item.getName());
-				itemsMap.putIfAbsent(item.getId(), item.getName());
+				itemsMap.putIfAbsent(item.getId(), item);
 			}
 		});
 
 		ScheduledExecutorService exec = Executors.newSingleThreadScheduledExecutor();
-		exec.scheduleAtFixedRate(new Runnable() {
-			@Override
-			public void run() {
-				//Here we need to now generate the random purchase thingy...
-				System.out.println(itemsMap.size());
-			}
+		exec.scheduleAtFixedRate(() -> {
+			Random generator = new Random();
+			Object[] itemsIds = itemsMap.keySet().toArray();
+			Object randomValue = itemsIds[generator.nextInt(itemsIds.length)];
+			//noinspection SuspiciousMethodCalls
+			Order newOrder = new Order()
+					.setItem(itemsMap.get(randomValue))
+					.setQuantity(generator.nextInt(Order.QUANTITY_MAX))
+					.setPrice(generator.nextFloat() * Order.PRICE_MAX);
+
+			producer.send(new ProducerRecord<>(sinkTopic, "purchase", gson.toJson(newOrder)));
+			logger.info("Sending " + newOrder.toString());
+
 		}, 5, 5, TimeUnit.SECONDS);
 
-		KafkaStreams streams = new KafkaStreams(builder.build(), props);
-
+		KafkaStreams streams = new KafkaStreams(builder.build(), sourceProps);
 		streams.start();
-
 	}
 }
