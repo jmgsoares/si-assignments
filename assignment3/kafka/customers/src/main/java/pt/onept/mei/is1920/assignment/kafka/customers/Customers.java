cfb3e627a9f4b52a9cf4af3994543a27fc8cd2ae
João Moreira <jmoreirawk@outlook.pt>
2019-12-10 18:40:28 +0000

    randomly picking an item and a country and sending to the topic

--- assignment3/kafka/customers/src/main/java/pt/onept/mei/is1920/assignment/kafka/customers/Customers.java
+++ assignment3/kafka/customers/src/main/java/pt/onept/mei/is1920/assignment/kafka/customers/Customers.java
@@ -1,6 +1,9 @@
 package pt.onept.mei.is1920.assignment.kafka.customers;
 
 import com.google.gson.Gson;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
@@ -10,10 +13,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pt.onept.mei.is1920.assignment.kafka.common.type.Country;
 import pt.onept.mei.is1920.assignment.kafka.common.type.Item;
+import pt.onept.mei.is1920.assignment.kafka.common.type.Sale;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -25,17 +30,29 @@ public class Customers {
 	public static void main(String[] args) {
 
 		String sourceTopic = "DBInfo";
+		String sinkTopic = "Sales";
 
-		Map<Long, String> itemsMap = new HashMap<>();
-		Map<Long, String> countriesMap = new HashMap<>();
+		Map<Long, Item> itemsMap = new HashMap<>();
+		Map<Long, Country> countriesMap = new HashMap<>();
 
 
 		java.util.Properties props = new Properties();
 		//props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-customers-app");
-		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-customers-test-app-1");
+		props.put(StreamsConfig.APPLICATION_ID_CONFIG, "kafkaShop-customers-test-app-2");
 		props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
 		props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+		Properties sinkProps = new Properties();
+		sinkProps.put("bootstrap.servers", "localhost:9092");
+		sinkProps.put("acks", "all");
+		sinkProps.put("retries", 0);
+		sinkProps.put("batch.size", 16384);
+		sinkProps.put("linger.ms", 1);
+		sinkProps.put("buffer.memory", 33554432);
+		sinkProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		sinkProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+		Producer<String, String> producer = new KafkaProducer<>(sinkProps);
 
 		StreamsBuilder builder = new StreamsBuilder();
 
@@ -48,11 +65,11 @@ public class Customers {
 				Item item = gson.fromJson(v, Item.class);
 				if(itemsMap.containsKey(item.getId())) {
 					logger.info("Updating item " + item.getId());
-					itemsMap.replace(item.getId(), item.getName());
+					itemsMap.replace(item.getId(), item);
 				}
 				else {
 					logger.info("Adding new item " + item.getId() + " " + item.getName());
-					itemsMap.putIfAbsent(item.getId(), item.getName());
+					itemsMap.putIfAbsent(item.getId(), item);
 				}
 			}
 			else if (k.equals("\"country\"")) {
@@ -60,11 +77,11 @@ public class Customers {
 				//noinspection DuplicatedCode
 				if(countriesMap.containsKey(country.getId())) {
 					logger.info("Updating country " + country.getId());
-					countriesMap.replace(country.getId(), country.getName());
+					countriesMap.replace(country.getId(), country);
 				}
 				else {
 					logger.info("Adding new country " + country.getId() + " " + country.getName());
-					countriesMap.putIfAbsent(country.getId(), country.getName());
+					countriesMap.putIfAbsent(country.getId(), country);
 				}
 			}
 		});
@@ -73,8 +90,27 @@ public class Customers {
 		exec.scheduleAtFixedRate(new Runnable() {
 			@Override
 			public void run() {
-				//Here we need to now generate the random sales thingy...
-				System.out.println(countriesMap.size() + " " + itemsMap.size());
+				// chose a random item and country
+				Random generator = new Random();
+				Object[] itemIds = itemsMap.keySet().toArray();
+				Object randomItem = itemIds[generator.nextInt(itemIds.length)];
+
+				Object[] countryIds = countriesMap.keySet().toArray();
+				Object randomCountry = countryIds[generator.nextInt(countryIds.length)];
+
+				// generate a random price
+
+				// generate a random quantity
+
+				Sale newSale = new Sale()
+						.setItem(itemsMap.get(randomItem))
+						.setPrice(generator.nextFloat() * Sale.PRICE_MAX)
+						.setQuantity(generator.nextInt(Sale.QUANTITY_MAX))
+						.setCountry(countriesMap.get(randomCountry));
+
+				producer.send(new ProducerRecord<>(sinkTopic, "sale", gson.toJson(newSale)));
+				logger.info("Sending " + newSale.toString());
+
 			}
 		}, 5, 5, TimeUnit.SECONDS);
 
