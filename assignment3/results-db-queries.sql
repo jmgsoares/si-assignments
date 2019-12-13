1ea8de385ca0d53ed4915052c0c04504d6aa45e7
João Soares <joao@soares.pro>
2019-12-13 17:49:34 +0000

    added the remaining function to interact with db as per requirements

--- assignment3/results-db-queries.sql
+++ assignment3/results-db-queries.sql
@@ -1,13 +1,77 @@
-select i.name, e.value from "Results"."ExpensePerItem" e
-    inner join items i
-        on cast(e.id as bigint)= i.id;
+-- R1.1 Insert country
+insert into countries (name) values ('USA');
 
-select i.name, a.value from "Results"."AverageExpenseByItem" a
-    inner join items i
-        on cast(a.id as bigint)=i.id;
+-- R1.2 Update country
+update countries
+set name=('Baguette')
+where name=('France');
 
-select a.value from "Results"."AverageExpenseByOrder" a;
+-- R2 List countries
+select name from countries order by name;
 
-select c.name, i.name, a.value2 from "Results"."CountryHighestSales" a
-inner join items i on cast(a.id as bigint)=i.id
-inner join countries c on cast(a.value as bigint)=c.id;
+-- R3.1 Insert item
+insert into items (name) values ('Key');
+
+-- R3.2 Update item
+update items
+set name=('KeyFob')
+where name=('Key');
+
+-- R4 List items
+select name from items order by name;
+
+-- R5 Revenue per item
+select i.name as item, r.value as value from "Results"."RevenuePerItem" r
+inner join items i
+on r.id=i.id
+order by value desc;
+
+-- R6 Expense per item
+select i.name as item, r.value as value from "Results"."ExpensePerItem" r
+inner join items i
+on r.id = i.id
+order by value desc;
+
+-- R7 Profit per item
+select i.name as item, r.value as value from "Results"."ProfitPerItem" r
+inner join items i
+on r.id = i.id
+order by value desc;
+
+-- R8 Total revenue
+select value from "Results"."TotalRevenue";
+
+-- R9 Total Expense
+select value from "Results"."TotalExpense";
+
+-- R10 Total profit
+select value from "Results"."TotalProfit";
+
+-- R11 Average expense per order (by item)
+select i.name as item, r.value as value from "Results"."AverageExpenseByItem" r
+inner join items i
+on r.id=i.id
+order by value desc;
+
+-- R12 Average expense per order
+select value from "Results"."AverageExpenseByOrder";
+
+-- R13 Item with the highest profit
+select i.name as name, r.value2 from "Results"."MostProfitableItem" r
+inner join items i
+on r.value = i.id;
+
+-- R14 Total revenue in the last hour
+select value from "Results"."TotalRevenueLastHour";
+
+-- R15 Total expense in the last hour
+select value from "Results"."TotalExpenseLastHour";
+
+-- R16 Total profit in the last hour
+select value from "Results"."TotalProfitLastHour";
+
+-- R17 Country of highest sales by item
+select i.name as item, c.name as country, r.value2 as value from "Results"."CountryHighestSales" r
+inner join items i on r.id = i.id
+inner join countries c on r.value = c.id
+order by value2 desc;
