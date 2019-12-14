46d1847301afe5620f945bebfdb28c434157f05b
João Moreira <jmoreirawk@outlook.pt>
2019-12-14 20:48:10 +0000

    Analytics cli complete

---
+++ assignment3/kafka/cli/src/main/java/pt/onept/mei/is1920/assignment/kafka/cli/CliHandler/AnalyticsOperationsHandler.java
@@ -0,0 +1,154 @@
+package pt.onept.mei.is1920.assignment.kafka.cli.CliHandler;
+
+import com.google.gson.JsonObject;
+import kong.unirest.GenericType;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
+import pt.onept.mei.is1920.assignment.kafka.cli.util.ConnectionUtility;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class AnalyticsOperationsHandler {
+    public static void analyticsOperationsMenu() {
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Type the number with the desired operation (0 to leave)");
+            System.out.println("1 - Get the revenue per item.");
+            System.out.println("2 - Get the expenses per item.");
+            System.out.println("3 - Get the profit per item.");
+            System.out.println("4 - Get the total revenues.");
+            System.out.println("5 - Get the total expenses.");
+            System.out.println("6 - Get the total profit.");
+            System.out.println("7 - Get the average amount spent in each purchase (separated by item).");
+            System.out.println("8 - Get the average amount spent in each purchase (aggregated for all items).");
+            System.out.println("9 - Get the item with the highest profit of all.");
+            System.out.println("10 - Get the total revenue in the last hour.");
+            System.out.println("11 - Get the total expenses in the last hour.");
+            System.out.println("12 - Get the total profits in the last hour.");
+            System.out.println("13 - Get the name of the country with the highest sales per item.");
+
+            int option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Revenue per item");
+                    getTotalPerItem("rev-itm");
+                    continue;
+                case 2:
+                    System.out.println("Expense per item");
+                    getTotalPerItem("exp-itm");
+                    continue;
+                case 3:
+                    System.out.println("Profit per item");
+                    getTotalPerItem("pft-itm");
+                    continue;
+                case 4:
+                    System.out.println("Total revenue");
+                    getTotal("tot-rev");
+                    continue;
+                case 5:
+                    System.out.println("Total expense");
+                    getTotal("tot-exp");
+                    continue;
+                case 6:
+                    System.out.println("Total profit");
+                    getTotal("tot-pft");
+                    continue;
+                case 7:
+                    System.out.println("Average amount spent in each purchase (separated by item)");
+                    getTotalPerItem("avg-exp-ord-itm");
+                    continue;
+                case 8:
+                    System.out.println("Average amount spent in each purchase (aggregated for all items)");
+                    getTotal("avg-exp-ord");
+                    continue;
+                case 9:
+                    getMostProfitableItem();
+                    continue;
+                case 10:
+                    System.out.println("Total revenue in the last hour");
+                    getTotal("lst-hour-rev");
+                    continue;
+                case 11:
+                    System.out.println("Total expense in the last hour");
+                    getTotal("lst-hour-exp");
+                    continue;
+                case 12:
+                    System.out.println("Total profit in the last hour");
+                    getTotal("lst-hour-pft");
+                    continue;
+                case 13:
+                    getCountryHighestSalesPerItem();
+                    continue;
+                default:
+                    return;
+            }
+        }
+    }
+
+    private static void getTotalPerItem(String param) {
+        JSONArray response = Unirest.get(ConnectionUtility.PAGE_URL_ANALYTICS + param)
+                .asJson()
+                .getBody()
+                .getArray();
+
+        if (!response.isEmpty()) {
+            for (Object o : response.toList()) {
+                System.out.println("Name: " + (((JSONObject) o).get("item").toString() +
+                        ", value: " + ((JSONObject) o).get("value").toString()));
+            }
+        }
+        System.out.println();
+    }
+
+    private static void getTotal(String param) {
+        JSONArray response = Unirest.get(ConnectionUtility.PAGE_URL_ANALYTICS + param)
+                .asJson()
+                .getBody()
+                .getArray();
+
+        for (Object o : response.toList()) {
+            System.out.println("Value: " + ((JSONObject) o).get("value").toString());
+        }
+        System.out.println();
+    }
+
+    private static void getMostProfitableItem() {
+        System.out.println("Item with the highest profit of all");
+        JSONArray response = Unirest.get(ConnectionUtility.PAGE_URL_ANALYTICS + "mst-prof-itm")
+                .asJson()
+                .getBody()
+                .getArray();
+
+        if (!response.isEmpty()) {
+            for (Object o : response.toList()) {
+                System.out.println("Name: " + (((JSONObject) o).get("name").toString() +
+                        ", value: " + ((JSONObject) o).get("value").toString()));
+            }
+        }
+        System.out.println();
+
+    }
+
+    private static void getCountryHighestSalesPerItem() {
+        JSONArray response = Unirest.get(ConnectionUtility.PAGE_URL_ANALYTICS + "cnt-hst-sale-itm")
+                .asJson()
+                .getBody()
+                .getArray();
+
+        System.out.println("Country with the highest sales per item");
+        if (!response.isEmpty()) {
+            for (Object o : response.toList()) {
+                System.out.println("Name: " + ((JSONObject) o).get("item").toString() +
+                        ", Country: " + ((JSONObject) o).get("country").toString() +
+                        ", Value: " + ((JSONObject) o).get("value").toString());
+            }
+        }
+        System.out.println();
+    }
+
+
+}
