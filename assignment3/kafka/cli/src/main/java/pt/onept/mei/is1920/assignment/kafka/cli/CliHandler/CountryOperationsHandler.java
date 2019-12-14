949b1090681c551fb44e220e5392b0db2c488896
João Moreira <jmoreirawk@outlook.pt>
2019-12-14 17:05:03 +0000

    created the list, add and edit functions

--- assignment3/kafka/cli/src/main/java/pt/onept/mei/is1920/assignment/kafka/cli/CliHandler/CountryOperationsHandler.java
+++ assignment3/kafka/cli/src/main/java/pt/onept/mei/is1920/assignment/kafka/cli/CliHandler/CountryOperationsHandler.java
@@ -1,19 +1,72 @@
 package pt.onept.mei.is1920.assignment.kafka.cli.CliHandler;
 
+import kong.unirest.GenericType;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import pt.onept.mei.is1920.assignment.kafka.cli.util.ConnectionUtility;
+
+import java.util.*;
+
 public class CountryOperationsHandler {
-    public static void countriesMenu() {
+    private static Scanner scanner = new Scanner(System.in);
 
-    }
+    public static void listCountries() {
+        int indexKey = 1; // starting at one
+        Map<Integer, String> countriesMap = new HashMap<>();
 
-    private static void listCountries() {
-        // GET
+        List<String> countries = Unirest.get(ConnectionUtility.PAGE_URL + "country")
+                .asObject(new GenericType<List<String>>() {})
+                .getBody();
+
+        if(!countries.isEmpty()) {
+            for (String c: countries) {
+                countriesMap.put(indexKey++, c);
+            }
+        }
+
+        if(countriesMap.isEmpty()) {
+            // If there are no countries to list, ask to add a new one.
+            System.out.println("No countries available. Add new country? (y/n)");
+            if(scanner.nextLine().equals("y")) {
+                addCountry();
+            }
+
+        } else {
+            // Else, allow the user to select one of the countries in order to edit it.
+            countriesMap.forEach((k, v) -> System.out.println(k + " " + v));
+
+            System.out.println("Edit a country? (y/n)");
+            if(scanner.nextLine().equals("y")) {
+                System.out.print("Select one country to edit (numbers only): ");
+                int option = scanner.nextInt();
+                scanner.nextLine(); // clean buffer
+                if(option > countriesMap.size() || option < 1) {
+                    System.out.println("Not a valid option.");
+                } else {
+                    editCountry(countriesMap.get(option));
+                }
+            }
+
+        }
+
     }
 
     private static void addCountry() {
-        // PUT
+        System.out.print("Name of the new country: ");
+        String newCountryName = scanner.nextLine();
+        // Send new name
+        HttpResponse<String> response = Unirest.post(ConnectionUtility.PAGE_URL + "country")
+                .queryString("name", newCountryName)
+                .asString();
     }
 
-    private static void editCountries() {
-        // POST
+    private static void editCountry(String selectedCountry) {
+        System.out.print("New name of the country: ");
+        String newCountryName = scanner.nextLine();
+        // Send old and new name
+        HttpResponse<String> response = Unirest.put(ConnectionUtility.PAGE_URL + "country")
+                .queryString("oldName", selectedCountry)
+                .queryString("newName", newCountryName)
+                .asString();
     }
 }
--- assignment3/kafka/cli/src/main/java/pt/onept/mei/is1920/assignment/kafka/cli/CliHandler/ItemsOperationsHandler.java
+++ assignment3/kafka/cli/src/main/java/pt/onept/mei/is1920/assignment/kafka/cli/CliHandler/ItemsOperationsHandler.java
@@ -1,19 +1,75 @@
 package pt.onept.mei.is1920.assignment.kafka.cli.CliHandler;
 
+import kong.unirest.GenericType;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import pt.onept.mei.is1920.assignment.kafka.cli.util.ConnectionUtility;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class ItemsOperationsHandler {
-    public static void itemsMenu() {
 
-    }
+    private static Scanner scanner = new Scanner(System.in);
 
-    private static void listItems() {
-        // GET
+    public static void listItems() {
+        int indexKey = 1; // starting at one
+        Map<Integer, String> itemsMap = new HashMap<>();
+
+        List<String> items = Unirest.get(ConnectionUtility.PAGE_URL + "item")
+                .asObject(new GenericType<List<String>>() {})
+                .getBody();
+
+        if(!items.isEmpty()) {
+            for (String c: items) {
+                itemsMap.put(indexKey++, c);
+            }
+        }
+
+        if(itemsMap.isEmpty()) {
+            // If there are no items to list, ask to add a new one.
+            System.out.println("No items available. Add new item? (y/n)");
+            if(scanner.nextLine().equals("y")) {
+                addItem();
+            }
+
+        } else {
+            // Else, allow the user to select one of the items in order to edit it.
+            itemsMap.forEach((k, v) -> System.out.println(k + " " + v));
+
+            System.out.println("Edit an item? (y/n)");
+            if(scanner.nextLine().equals("y")) {
+                System.out.print("Select one item to edit (numbers only): ");
+                int option = scanner.nextInt();
+                scanner.nextLine(); // clean buffer
+                if(option > itemsMap.size() || option < 1) {
+                    System.out.println("Not a valid option.");
+                } else {
+                    editItem(itemsMap.get(option));
+                }
+            }
+
+        }
     }
 
     private static void addItem() {
-        // PUT
+        System.out.print("Name of the new item: ");
+        String newItemName = scanner.nextLine();
+        // Send new name
+        HttpResponse<String> response = Unirest.post(ConnectionUtility.PAGE_URL + "item")
+                .queryString("name", newItemName)
+                .asString();
     }
 
-    private static void editItems() {
-        // POST
+    private static void editItem(String selectedItem) {
+        System.out.print("New name of the item: ");
+        String newItemName = scanner.nextLine();
+        // Send old and new name
+        HttpResponse<String> response = Unirest.put(ConnectionUtility.PAGE_URL + "item")
+                .queryString("oldName", selectedItem)
+                .queryString("newName", newItemName)
+                .asString();
     }
 }
